36f21e64ebe889ea6840ca3c21d47cc7dda946cf
Tino <tino@rover-246-169.rovernet.mtu.edu>
2017-04-16 19:28:12 -0400

    worked on TGS REQ/REP

--- KDC.ino
+++ KDC.ino
@@ -9,7 +9,7 @@
 XBee xbee= XBee();
 SoftwareSerial mySerial(10, 11); // RX, TX
 uint8_t* rxPayload;
-uint8_t txPayload[3+3*N_BLOCK];
+uint8_t txPayload[3+4*N_BLOCK];
 byte currentNode;
 uint16_t address;
 ZBRxResponse pck = ZBRxResponse();
@@ -56,18 +56,19 @@ void loop() {
        opcode = rxPayload[0];
 
        switch (opcode){
-          case 0: 
+          case 0: {
+            Serial.println("Processing opcode " + 0);
           //Client login. 
           //Invent a 128' session key 
           //loopup clients master key using rxPayload[2]
-            //assert(rxPayload[1] == KRB_AS_REQ);
-    /*debug*/ Serial.println(rxPayload[1]);
+            assert(rxPayload[1] == KRB_AS_REQ);
+            /*debug*/ Serial.println(rxPayload[1]);
             //establish session key
-            byte sessionKey[N_BLOCK]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'};
+            byte sessionKey[N_BLOCK];//= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'};
             int i;
-            //for (i = 0; i < 16; i ++){
-            //  sessionKey[i] = (byte)random(0, 255);
-            //}
+            for (i = 0; i < N_BLOCK; i ++){
+              sessionKey[i] = (byte)random(0, 255);
+            }
             
             //encrypt session key with clients master key
             byte cipherTxt[N_BLOCK];
@@ -88,7 +89,7 @@ void loop() {
             aes.encrypt(tgt.sessionKey, cipherTxt);
             memcpy(&txPayload[2+2*N_BLOCK], cipherTxt, sizeof(cipherTxt));
 
-            /*debug*/ Serial.println(rxPayload[2]);
+            /*debug*/ //Serial.println(rxPayload[2]);
             XBeeAddress64 addr64 = XBeeAddress64(highAddress[rxPayload[2]],lowAddress[rxPayload[2]]);
             ZBTxRequest tx = ZBTxRequest(addr64, txPayload, sizeof(txPayload));
             tx.setAddress16(0xfffe);
@@ -97,7 +98,64 @@ void loop() {
             Serial.println(highAddress[rxPayload[2]]);
             Serial.println(lowAddress[rxPayload[2]]);
             break;
+          }
+          case 2 : {
+            //extract desired resource and nonce
+            //extract tgt
+            //build TGS_REP for client
+            //generate a random session key
+            //create ticket for requested resource. This conisists of the client initiating contact and the session key both encrypted with the resources master key
 
+            Serial.println("Processing Opcode " + 2);
+            assert(rxPayload[1] == KRB_TGS_REQ);
+            /*debug*/ Serial.println((int)rxPayload[1]);
+            //establish session key
+            byte sessionKey[N_BLOCK]; //= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'};
+            int i;
+            for (i = 0; i < N_BLOCK; i ++){
+              sessionKey[i] = (byte)random(0, 255);
+            }
+
+            //grag the tgt
+            TGT tgt;
+            
+            aes.set_key(masterKeys[0], 128);
+            aes.decrypt(&rxPayload[2], tgt.clientName);
+            aes.decrypt(&rxPayload[2 + N_BLOCK], tgt.sessionKey);
+
+            byte resourceInfo[N_BLOCK];
+            aes.set_key(tgt.sessionKey, 128);
+            aes.decrypt(&rxPayload[2+2*N_BLOCK], resourceInfo);
+
+            //Construct response
+            memset(txPayload, 0, sizeof(txPayload));
+            txPayload[0] = (byte)3; //set opcode to KRB_AS_REP
+            txPayload[1] = KRB_TGS_REP;
+            byte cipherTxtA[4*N_BLOCK];
+            byte cipherTxtB[2*N_BLOCK];
+            aes.set_key(masterKeys[resourceInfo[0]], 128);//set to bob's key
+            aes.encrypt(tgt.clientName, &cipherTxtB[0]);
+            aes.encrypt(sessionKey, &cipherTxtB[N_BLOCK]);
+            
+            aes.set_key(tgt.sessionKey, 128); //set to alice's key
+            aes.encrypt(resourceInfo, &cipherTxtA[0]);
+            aes.encrypt(sessionKey, &cipherTxtA[N_BLOCK]);
+            aes.encrypt(&cipherTxtB[0], &cipherTxtA[2*N_BLOCK]);
+            aes.encrypt(&cipherTxtB[N_BLOCK], &cipherTxtA[3*N_BLOCK]);
+
+            //build tx packet and send to alice
+            memcpy(&txPayload[2], cipherTxtA, sizeof(cipherTxtA));
+            XBeeAddress64 addr64 = XBeeAddress64(highAddress[tgt.clientName[0]],lowAddress[tgt.clientName[0]]);
+            ZBTxRequest tx = ZBTxRequest(addr64, txPayload, sizeof(txPayload));
+            tx.setAddress16(0xfffe);
+            xbee.send(tx);
+            Serial.println("TGS response sent");
+            Serial.println(highAddress[rxPayload[2]]);
+            Serial.println(lowAddress[rxPayload[2]]);
+            //Serial.print("K_AB = "); Serial.println(sessionKey);
+            
+            break;
+          }
 
 
         
